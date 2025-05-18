376a4bc5bb01fa5b299eb22ee9f82453f8bac981
ledessma-g <gabrielledesmadiaz@gmail.com>
2025-05-18 22:23:57 +0000

    feat: add astro paths and new font fix: assets directory location

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,17 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        resolve: {
+            alias: {
+                '@components': '/src/components',
+                '@layouts': '/src/layouts',
+                '@pages': '/src/pages',
+                '@public': '/public',
+                '@styles': '/src/styles',
+                '@font': '/public/assets/font'
+            }
+        }
+    }
+});
